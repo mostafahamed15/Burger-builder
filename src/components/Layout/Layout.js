de0222fa03f0b400be0a11970da9559eb07e2767
Mostafa <mostafahamed73@yahoo.com>
2018-08-31 18:01:32 +0200

    add style to layout component

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import Aux from '../../hoc/Auxl';
+import classes from './Layout.css';
 const layout =  (props) => (
     <Aux>
     <div>Toolbar, SideDrower, backDrop</div>
-    <main>
+    <main className={classes.Content} >
         {props.children}
     </main>
     </Aux>
